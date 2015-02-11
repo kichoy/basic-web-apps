22cc7d12e7f5bc8ec0312cd1213eaa5985e088e0
kichoy <kichoy@s.sfusd.edu>
2015-02-11 00:30:08 +0000

    initial commit, changes to all 3 ruby apps

--- bake-app.rb
+++ bake-app.rb
@@ -3,14 +3,35 @@ require "sinatra"     # Load the Sinatra web framework
 get("/") do
   html = ""
 
-  html.concat("<h1>Hello, World!</h1>")
+  html.concat("<h1>Hello, fsfsf!</h1>")
   html.concat("<ul>")
+  html.concat('<style> body {background-color: #33CC66;} </style>')
   html.concat("<li><a href='/waffles'>show me waffles</a></li>")
+  html.concat("<li><a href='/pancakes'>show me pancakes</a></li>")
+  html.concat("<li><a href='/santa'>show me santa</a></li>")
   html.concat("<li><a href='/waffles/chocolate'>show me chocolate</a></li>")
   html.concat("<li><a href='/bake?baked_good=cookies&count=10'>bake 10 cookies</a></li>")
-  html.concat("<li><a href='/bake?baked_good=cronut&count=5'>bake 5 cronuts</a></li>")
-  html.concat("<li><a href='/bake?baked_good=cupcakes&count=1138'>bake 1138 cupcakes</a></li>")
+  html.concat("\n<li><a href='/bake?baked_good=cronut&count=5' onmouseover='alert(this.href)'>bake 5 cronuts</a></li>")
+  html.concat("\n<li><a href='/bake?baked_good=cupcakes&count=1138' onmouseover='this.href=this.href+\"9\"'>bake 1138 cupcakes</a></li>")
+  html.concat("\n<li><a href='/bake?baked_good=santas&count=1'>bake over 900 santas</a></li>")
+  html.concat("<li><a href='/eat?animal=santas&count=3'>eat 3 santas</a></li>")
   html.concat("</ul>")
+  
+  html.concat('<xstyle>
+    body {
+        background-color: #d0e4fe;
+    }
+    
+    h1 {
+        color: orange;
+        text-align: center;
+    }
+    
+    p {
+        font-family: "Times New Roman";
+        font-size: 20px;
+    }
+  </xstyle>')
 
   body(html)
 end
@@ -22,6 +43,17 @@ get("/waffles") do
 
   body(html)
 end
+
+get("/santa") do
+  html = ""
+  
+  html.concat('<h1 style="color:red; text-align: center"> Santa is delicious.</h1>')
+  html.concat('<img src="http://www.turnbacktogod.com/wp-content/uploads/2008/12/santa-claus-pics-0101.jpg" alt="santa is delicious" text-align:"center">')
+  html.concat('<style> body {background-color: #33CC66;}')
+
+  body(html)
+end
+
 
 get("/waffles/chocolate") do
   html = ""
@@ -42,6 +74,22 @@ get("/bake") do
   html.concat("<ul>")
   count.times do |num|
     html.concat("<li>#{baked_good} number #{num}</li>")
+  end
+  html.concat("</ul>")
+
+  body(html)
+end
+
+# Visit, e.g., /eat?animal=waffles&count=20
+get("/eat") do
+  count = Integer(params["count"])
+  animal = String(params["animal"])
+
+  html = "I'm going to eat #{count} #{animal}!"
+
+  html.concat("<ul>")
+  count.times do |num|
+    html.concat("<li>#{animal} number #{num}</li>")
   end
   html.concat("</ul>")
 
